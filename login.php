85434068c6672a4f87b1af5d8bc98c92104c31b7
Piyawan08 <91970247+Piyawan08@users.noreply.github.com>
2022-10-20 10:40:23 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>RFID</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="csso/images/icons/11.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="csso/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="csso/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="csso/fonts/iconic/css/material-design-iconic-font.min.css">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&family=Prompt&display=swap" rel="stylesheet">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="csso/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="csso/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="csso/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="csso/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="csso/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="csso/css/util.css">
+	<link rel="stylesheet" type="text/css" href="csso/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100" >
+			<div class="wrap-login100">
+				<form class="login100-form validate-form" action="login_db.php" method="post">
+					
+
+					<span class="login100-form-title p-b-34 p-t-27">
+						ระบบบันทึกข้อมูลบัตรเฝ้าไข้
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" for="username" type="text" name="username" placeholder="Username">
+						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" for="username" type="password" name="password" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xf191;"></span>
+					</div>
+
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" name="login_user">
+							เข้าสู่ระบบ
+						</button>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="csso/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="csso/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="csso/vendor/bootstrap/js/popper.js"></script>
+	<script src="csso/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="csso/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="csso/vendor/daterangepicker/moment.min.js"></script>
+	<script src="csso/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="csso/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="csso/js/main.js"></script>
+
+</body>
+</html>
